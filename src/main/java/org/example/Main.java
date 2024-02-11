88e9cc47c9aab9b05c92b16fa5e9906ae1ab422e
momotofff <momotofff@mail.ru>
2024-02-11 09:16:38 +0400

    продолжаю разбирать json

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,13 @@
 package org.example;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.awt.*;
+import java.io.File;
 import java.io.IOException;
+
+import java.util.ArrayList;
 
 
 public class Main
@@ -27,6 +31,31 @@ public class Main
             Magician newMagician = objectMapper.readValue(result, Magician.class);
             System.out.println(comparison(magician, newMagician) ? "Ну так то заебись" : "чёта хуйня пошла");
 
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+        ArrayList<Entity> arrayMagician = new ArrayList<>();
+        for (int i = 0; i < 5; ++i)
+            arrayMagician.add(new Magician(new Point((int) (Math.random() * i), (int) (Math.random() * i)), "маг №" + i));
+        try {
+            result = objectMapper.writeValueAsString(arrayMagician);
+            objectMapper.writeValue(new File("target/arrayMagician.json"), arrayMagician);
+            System.out.println(result);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        File file = new File("target/arrayMagician.json");
+
+        try {
+            ArrayList<Magician> list = objectMapper.readValue(file, new TypeReference<ArrayList<Magician>>(){});
+            for (int i = 0; i < list.size(); ++i)
+                System.out.println(comparison(arrayMagician.get(i), list.get(i)));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
